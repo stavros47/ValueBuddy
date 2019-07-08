60c96ad659716bafacd25f74dd70055760f4dcb2
Stavros <stavris.47@gmail.com>
2019-07-08 16:50:51 +0300

    add body parser middleware and remove view engine

--- server/app.js
+++ server/app.js
@@ -1,23 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const createError = require('http-errors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser')
 
-var app = express();
+/* Include the express framework and invoke it*/
+const express = require('express');
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+var indexRouter = require('./routes');
+app.use('/', indexRouter);
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
