cda2a2b813701a6d5b1308fc53544f052f989e6a
Stavros <stavris.47@gmail.com>
2019-07-08 19:28:30 +0300

    Route GET /Customers/:id/Coupons Implemented

--- server/routes/customers.js
+++ server/routes/customers.js
@@ -5,25 +5,41 @@ const database = require('../database');
 /* GET home page. */
 router.get('/', function(req, res, next) { 
   database.raw('SELECT * FROM get_customers()').then(data =>{
-    res.json(data.rows);
+    if(data.rows === undefined || data.rows.length == 0){
+      res.json({message:"Customers not Found!", customers:[]});        
+    }else{
+      res.json({customers:data.rows});
+    }
   });    
 
 });
 
-/* GET home page. */
+/* GET a Customer */
 router.get('/:id', function(req, res, next) { 
     // database.raw(`SELECT * FROM get_customer($1)`,req.body.id).then(data =>{
     database.raw(`SELECT * FROM get_customer(${parseInt(req.params.id)})`).then(data =>{
       if(data.rows === undefined || data.rows.length == 0){
-        res.send({message:"User not Found!"});        
+        res.json({message:"Customer not Found!", customer:{}});        
       }else{
-        res.send(data.rows);
-        console.log(data.rows);
-      }
-      
+        res.json({customer:data.rows});       
+      }      
     });    
   
-  });
+});
+
+/* GET a Customer's Coupons. */
+router.get('/:id/Coupons', function(req, res, next) { 
+  // database.raw(`SELECT * FROM get_customer($1)`,req.body.id).then(data =>{
+  database.raw(`SELECT * FROM get_customers_coupons(${parseInt(req.params.id)})`).then(data =>{
+    if(data.rows === undefined || data.rows.length == 0){
+      res.json({message:"Customer did not have any coupons claimed!", coupons:[]});        
+    }else{
+      res.json({coupons:data.rows});       
+    }      
+  });    
+
+});
+
 
 
 module.exports = router;
