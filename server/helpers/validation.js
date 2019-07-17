bd7269f0a04012883f0deea463dd727056517490
Stavros <stavris.47@gmail.com>
2019-07-17 20:11:42 +0300

    Added a null check to prevent some errors

--- server/helpers/validation.js
+++ server/helpers/validation.js
@@ -40,25 +40,28 @@ function getBusinessByUserID(user_id){
     return database('business').where('user_id', user.user_id).select('business_id').first()
 }
 
-function getRole(email){
+function getRole(email){    
     return database('users').where('email',email).first()
     .then(user =>{
-        return database('customer').where('user_id', user.user_id).select('customer_id')
-        .then(customerID =>{
-            return database('business').where('user_id', user.user_id).select('business_id')
-            .then(businessID =>{  
-                if(customerID[0]){
-                    let {customer_id} = customerID[0];
-                    return {...user, role:'customer', role_id:customer_id}
-                }else if(businessID[0]){
-                    let {business_id} = businessID[0];
-                    return {...user, role:'business', role_id:business_id}
-                }          
-                    
+        if(user){
+            return database('customer').where('user_id', user.user_id).select('customer_id')
+            .then(customerID =>{
+                return database('business').where('user_id', user.user_id).select('business_id')
+                .then(businessID =>{  
+                    if(customerID[0]){
+                        let {customer_id} = customerID[0];
+                        return {...user, role:'customer', role_id:customer_id}
+                    }else if(businessID[0]){
+                        let {business_id} = businessID[0];
+                        return {...user, role:'business', role_id:business_id}
+                    }          
+                        
+                }).catch(e => console.log(e));
             }).catch(e => console.log(e));
-        }).catch(e => console.log(e));
-    }).catch(e => console.log(e));    
-           
+        }else{
+            return;
+        }
+    }).catch(e => console.log(e));        
 }
 
 module.exports = {
