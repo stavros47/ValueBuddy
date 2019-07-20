5edfee5cf34c0ac9fc8e2bfbf8a671eb466d4191
Stavros <stavris.47@gmail.com>
2019-07-20 23:47:45 +0300

    Create Login/logout handlers and convert to class component/add state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,52 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
+import Dashboard from './Components/Dashboard';
 import NavBar from './Components/NavBar';
-import UserLogin from './Components/UserLogin/UserLogin';
+import UserLogin from './Components/UserLogin';
 import UserRegistration from './Components/UserRegistration';
+import {PrivateRoute} from './Components/PrivateRoute';//Higher order component
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar/>
-      <Route exact path='/' component={UserLogin}/>
-      <Route exact path='/signup' component={UserRegistration}/>
-    </div>
-  );
+import AuthHelperMethods from './Components/AuthHelperMethods';
+const Auth = new AuthHelperMethods("http://localhost:3001"); 
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      currentUser: Auth.loggedIn() ? Auth.getConfirm() : null,
+      isAuthed: Auth.loggedIn()
+    }    
+    this.handleLogout = this.handleLogout.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin(result){
+    if(result){
+      this.setState({currentUser:Auth.getConfirm(), isAuthed:true});
+    }
+  }
+
+  handleLogout(e){
+    e.preventDefault();
+
+    Auth.logout();
+    this.setState({currentUser:Auth.loggedIn() ? Auth.getConfirm() : null, isAuthed:false});
+  }
+ 
+  render() {
+
+    return (
+      <BrowserRouter>    
+        <div className="App">
+          <NavBar user={this.state.currentUser} handleLogout={this.handleLogout}/>
+          <PrivateRoute exact path ='/' component={Dashboard} isAuthed={this.state.isAuthed}/>
+          <Route exact path='/login' render={(props) => <UserLogin {...props} isAuthed={this.state.isAuthed} handleLogin={this.handleLogin}/>}/>
+          <Route exact path='/signup' component={UserRegistration}/>
+        </div>
+      </BrowserRouter>
+    );
+  }
+
 }
 
 export default App;
