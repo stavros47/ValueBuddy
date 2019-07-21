37bf6410b9f47df4bb0c19cb1385dfdbff3c11b5
Stavros <stavris.47@gmail.com>
2019-07-21 18:43:06 +0300

    Refactor and add different appbar when user is not logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 import Dashboard from './Components/Dashboard';
-import NavBar from './Components/NavBar';
-import UserLogin from './Components/UserLogin';
 import UserRegistration from './Components/UserRegistration';
 import {PrivateRoute} from './Components/PrivateRoute';//Higher order component
+import BasicAppBar from './Components/BasicAppBar';
+import Login from './Components/Login';
 
 import AuthHelperMethods from './Components/AuthHelperMethods';
 const Auth = new AuthHelperMethods("http://localhost:3001"); 
@@ -13,7 +13,7 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      currentUser: Auth.loggedIn() ? Auth.getConfirm() : null,
+      currentUser: Auth.loggedIn() ? Auth.getTokenData() : null,
       isAuthed: Auth.loggedIn()
     }    
     this.handleLogout = this.handleLogout.bind(this);
@@ -22,7 +22,7 @@ class App extends React.Component {
 
   handleLogin(result){
     if(result){
-      this.setState({currentUser:Auth.getConfirm(), isAuthed:true});
+      this.setState({currentUser:Auth.getTokenData(), isAuthed:true});
     }
   }
 
@@ -30,7 +30,7 @@ class App extends React.Component {
     e.preventDefault();
 
     Auth.logout();
-    this.setState({currentUser:Auth.loggedIn() ? Auth.getConfirm() : null, isAuthed:false});
+    this.setState({currentUser:Auth.loggedIn() ? Auth.getTokenData() : null, isAuthed:false});
   }
  
   render() {
@@ -38,9 +38,16 @@ class App extends React.Component {
     return (
       <BrowserRouter>    
         <div className="App">
-          <NavBar user={this.state.currentUser} handleLogout={this.handleLogout}/>
-          <PrivateRoute exact path ='/' component={Dashboard} isAuthed={this.state.isAuthed}/>
-          <Route exact path='/login' render={(props) => <UserLogin {...props} isAuthed={this.state.isAuthed} handleLogin={this.handleLogin}/>}/>
+          {!this.state.isAuthed &&
+            <BasicAppBar/>
+          }
+          <PrivateRoute exact path ='/' 
+            component={Dashboard}
+            user={this.state.currentUser}
+            handleLogout={this.handleLogout}
+            isAuthed={this.state.isAuthed}            
+          />
+          <Route exact path='/login' render={(props) => <Login {...props} handleLogin={this.handleLogin}/>}/>
           <Route exact path='/signup' component={UserRegistration}/>
         </div>
       </BrowserRouter>
