33d9edacf2ee22187630606d743be138ab881d2e
Stavros <stavris.47@gmail.com>
2019-07-24 20:18:06 +0300

    From .js to .jsx

--- frontend/src/Components/BatchItem.jsx
+++ frontend/src/Components/BatchItem.jsx
@@ -3,7 +3,6 @@ import React, {useState} from 'react';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { IconButton } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
