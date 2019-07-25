958199becab0292cca1f16beb072ab7779648169
Stavros <stavris.47@gmail.com>
2019-07-25 15:46:33 +0300

    Create the Coupon item component

--- frontend/src/Components/CouponItem.jsx
+++ frontend/src/Components/CouponItem.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
@@ -19,20 +19,25 @@ export default function CouponItem(props) {
             subheader={`By: ${coupon.business_name}`}
             className="card-header"/>
            
-            <Grid container direction="row" justify="space-between" alignItems="center" className="card_content" spacing={0}>       
-                <Grid item xs={12} sm={4} md={4} zeroMinWidth >
+            <Grid container direction="row" justify="space-between" alignItems="center" className="card_content" spacing={0}>
+            <Grid item xs={12} sm={4} md={3} zeroMinWidth >
+                    <Typography variant="subtitle1" align="center">
+                        Owner: <span style={{display:'block'}}>{coupon.customer_name}</span>
+                    </Typography>
+                </Grid>       
+                <Grid item xs={12} sm={4} md={3} zeroMinWidth >
                     <Typography variant="subtitle1" align="center">
                         Category: <span style={{display:'block'}}>{coupon.coupon_type}</span>
                     </Typography>
                 </Grid>
-                <Grid item xs={12} sm={4} md={4} zeroMinWidth >
+                <Grid item xs={12} sm={4} md={3} zeroMinWidth >
                     <Typography variant="subtitle1" align="center">
                        Starts: <span style={{display:'block'}}>{coupon.start_date.split('T')[0]}</span>
                     </Typography>
                 </Grid>
-                <Grid item xs={12} sm={4} md={4} zeroMinWidth >
+                <Grid item xs={12} sm={4} md={3} zeroMinWidth >
                     <Typography variant="subtitle1" align="center">
-                        Expires: <span style={{display:'block'}}>{coupon.expirty_date.split('T')[0]}</span>
+                        Expires: <span style={{display:'block'}}>{coupon.expiry_date.split('T')[0]}</span>
                     </Typography>
                 </Grid>
              </Grid>                               
