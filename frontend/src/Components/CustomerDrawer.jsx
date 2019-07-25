2851fdfa2cca93fa581db409078575541fcf1862
Stavros <stavris.47@gmail.com>
2019-07-25 15:47:17 +0300

    Make the Drawer link to appropreate pages

--- frontend/src/Components/CustomerDrawer.jsx
+++ frontend/src/Components/CustomerDrawer.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+
+
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -6,15 +9,34 @@ import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 
+const drawerItems = [
+  {
+    name:'Dashboard',
+    path:'/',
+  },
+  {
+    name:'My Coupons',
+    path:'/Coupons',
+  },
+  {
+    name:'Discover',
+    path:'/AllCoupons',
+  },
+
+  
+]
+
 export default function CustomerDrawer(props) {
     
     return (        
         <List>
-        {['Profile', 'My Coupons', 'Discover', 'Test'].map((text, index) => (
-          <ListItem button key={text}>
+        {drawerItems.map((drawerItem, index) => (
+          <RouterLink to={drawerItem.path} key={drawerItem.path}>
+          <ListItem button >
             <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
+            <ListItemText primary={drawerItem.name} />
           </ListItem>
+          </RouterLink>
         ))}
       </List>
     );
