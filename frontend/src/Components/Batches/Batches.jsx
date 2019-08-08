dbc85e5d20be88acd54413a012b59a51109a5de0
Stavros <stavris.47@gmail.com>
2019-08-09 02:30:45 +0300

    Fix dates - POST new Batch

--- frontend/src/Components/Batches/Batches.jsx
+++ frontend/src/Components/Batches/Batches.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 
+//Material-ui components
 import { Grid, Typography, Fab, Hidden } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import { makeStyles } from "@material-ui/core/styles";
-
-import AuthHelperMethods from "../AuthHelperMethods";
+//Components
 import BatchItem from "./BatchItem";
 import DialogNewBatch from "./DialogNewBatch";
+
+//Helpers
+import AuthHelperMethods from "../AuthHelperMethods";
 
 const Auth = new AuthHelperMethods("http://localhost:3001");
 
@@ -33,11 +36,14 @@ export default function Batches(props) {
     setOpen(false);
   }
 
+  /*
+   - Dates need to be in a certain format to be acceptable by input type="date"
+  */
   const [newBatch, setNewBatch] = useState({
     template_id: "",
     created_count: "",
-    start_date: new Date(),
-    expiry_date: new Date(),
+    start_date: new Date().toISOString().substring(0, 10),
+    expiry_date: new Date().toISOString().substring(0, 10),
     status_id: ""
   });
 
@@ -45,7 +51,6 @@ export default function Batches(props) {
     const target = event.target;
     const value = target.value;
     const name = target.name;
-
     setNewBatch({
       ...newBatch,
       [name]: value
@@ -56,22 +61,22 @@ export default function Batches(props) {
     event.preventDefault();
     handleClose();
 
-    console.log(newBatch);
-    // Auth.fetch({
-    //   method: "post",
-    //   url: `http://localhost:3001/${props.resourcePath}/Batches`,
-    //   data: { ...newBatch }
-    // })
-    //   .then(res => {
-    //     console.log(res);
-    //     if (res.batch) {
-    //       setBatches([...batches, res.batch[0]]);
-    //       console.log("+batches:", batches);
-    //     }
-    //   })
-    //   .catch(e => {
-    //     console.log(e);
-    //   });
+      method: "post",
+      url: `http://localhost:3001/${props.resourcePath}/Templates/${
+        newBatch.template_id
+      }/Batches`,
+      data: { ...newBatch }
+    })
+      .then(res => {
+        console.log(res);
+        if (res.batch) {
+          setBatches([...batches, res.batch[0]]);
+          console.log("+batches:", batches);
+        }
+      })
+      .catch(e => {
+        console.log(e);
+      });
   };
 
   useEffect(() => {
