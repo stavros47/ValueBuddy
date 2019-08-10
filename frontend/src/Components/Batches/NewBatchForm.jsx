15d706f5df012fc1a5e7e193e7eb4ba247e34ece
Stavros <stavris.47@gmail.com>
2019-08-10 19:38:59 +0300

    Add Grid - not yet responsive though

--- frontend/src/Components/Batches/NewBatchForm.jsx
+++ frontend/src/Components/Batches/NewBatchForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import {
+  Grid,
   MenuItem,
   DialogContent,
   DialogContentText,
@@ -45,80 +46,90 @@ export default function NewBatchForm(props) {
           Create a new Coupon Batch.
         </DialogContentText>
         <form className={classes.dialog} autoComplete="off">
-          <TextField
-            select
-            required
-            fullWidth
-            margin="dense"
-            name="template_id"
-            value={props.newBatch.template_id}
-            onChange={props.handleInputChange}
-            id="template_id"
-            label="Template"
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="flex-start"
+            spacing={4}
           >
-            {props.templates.map(template => {
-              return (
-                <MenuItem
-                  key={template.template_id}
-                  name={template.template_id}
-                  value={template.template_id.toString()}
-                >
-                  {template.description}
-                </MenuItem>
-              );
-            })}
-          </TextField>
-          <TextField
-            name="start_date"
-            value={props.newBatch.start_date}
-            onChange={props.handleInputChange}
-            label="Start Date"
-            type="date"
-            className={classes.textField}
-            InputLabelProps={{
-              shrink: true
-            }}
-          />
-          <TextField
-            name="expiry_date"
-            value={props.newBatch.expiry_date}
-            onChange={props.handleInputChange}
-            label="Expiry Date"
-            type="date"
-            className={classes.textField}
-            InputLabelProps={{
-              shrink: true
-            }}
-          />
+            <Grid item xs={12} sm={12} md={12}>
+              <TextField
+                select
+                required
+                fullWidth
+                margin="dense"
+                name="template_id"
+                value={props.newBatch.template_id}
+                onChange={props.handleInputChange}
+                id="template_id"
+                label="Template"
+              >
+                {props.templates.map(template => {
+                  return (
+                    <MenuItem
+                      key={template.template_id}
+                      name={template.template_id}
+                      value={template.template_id.toString()}
+                    >
+                      {template.description}
+                    </MenuItem>
+                  );
+                })}
+              </TextField>
+            </Grid>
+            <TextField
+              name="start_date"
+              value={props.newBatch.start_date}
+              onChange={props.handleInputChange}
+              label="Start Date"
+              type="date"
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
+            <TextField
+              name="expiry_date"
+              value={props.newBatch.expiry_date}
+              onChange={props.handleInputChange}
+              label="Expiry Date"
+              type="date"
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
 
-          <TextField
-            required
-            margin="dense"
-            name="created_count"
-            value={props.newBatch.created_count}
-            onChange={props.handleInputChange}
-            id="created_count"
-            label="Count"
-            type="text"
-          />
-          <TextField
-            select
-            required
-            style={{ width: "100px" }}
-            margin="dense"
-            name="status_id"
-            value={props.newBatch.status_id}
-            onChange={props.handleInputChange}
-            id="status_id"
-            label="Status"
-          >
-            <MenuItem selected value={4}>
-              Inactive
-            </MenuItem>
-            <MenuItem value={"1"}>Active</MenuItem>
-            <MenuItem value={"2"}>Expired</MenuItem>
-            <MenuItem value={"3"}>Disabled</MenuItem>
-          </TextField>
+            <TextField
+              required
+              margin="dense"
+              name="created_count"
+              value={props.newBatch.created_count}
+              onChange={props.handleInputChange}
+              id="created_count"
+              label="Count"
+              type="text"
+            />
+            <TextField
+              select
+              required
+              style={{ width: "100px" }}
+              margin="dense"
+              name="status_id"
+              value={props.newBatch.status_id}
+              onChange={props.handleInputChange}
+              id="status_id"
+              label="Status"
+            >
+              <MenuItem selected value={4}>
+                Inactive
+              </MenuItem>
+              <MenuItem value={"1"}>Active</MenuItem>
+              <MenuItem value={"2"}>Expired</MenuItem>
+              <MenuItem value={"3"}>Disabled</MenuItem>
+            </TextField>
+          </Grid>
         </form>
       </DialogContent>
     </div>
