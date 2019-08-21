47a07a70174bbaa6ffb1342ef6a0477b391db467
Stavros <stavris.47@gmail.com>
2019-08-21 11:30:16 +0300

    Display a message when no results are found

--- frontend/src/Components/BrowseCoupons/Category.jsx
+++ frontend/src/Components/BrowseCoupons/Category.jsx
@@ -1,67 +1,86 @@
-import React, { useState, useEffect } from 'react'
-import { Link as RouterLink } from 'react-router-dom'
-import Link from '@material-ui/core/Link'
-import { Grid, Typography, Tooltip, IconButton } from '@material-ui/core'
-import ArrowBack from '@material-ui/icons/ArrowBack'
-import CouponInstance from './CouponInstance'
-import AuthHelperMethods from '../AuthHelperMethods'
+import React, { useState, useEffect } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
+import { Grid, Typography, Tooltip, IconButton } from '@material-ui/core';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import CouponInstance from './CouponInstance';
+import AuthHelperMethods from '../AuthHelperMethods';
 
-const Auth = new AuthHelperMethods('http://localhost:3001')
+const Auth = new AuthHelperMethods('http://localhost:3001');
+
+const categories = {
+  Food: 1,
+  Clothing: 2,
+  Drinks: 3,
+  Technology: 4,
+  Groceries: 5,
+  Coffee: 6,
+};
 
 export default function Category(props) {
   // console.log('Category props: ',props);
-  const { match, location } = props
-  const [batches, setBatches] = useState([])
+  const { match, location } = props;
+  const [batches, setBatches] = useState([]);
 
   useEffect(() => {
     Auth.fetch({
       method: 'get',
-      url: `http://localhost:3001/Batches?categoryID=${
-        location.state.categoryID
-      }`,
+      url: `http://localhost:3001/Batches?categoryID=${categories[match.params.categoryName]}`,
       data: {},
     }).then(res => {
       if (res && res.batches) {
-        console.log(res.batches)
-        setBatches(res.batches)
+        console.log(res.batches);
+        setBatches(res.batches);
       } else {
-        console.log('There are no available coupons for this category')
+        console.log('There are no available coupons for this category');
       }
-    })
-  }, [location.state.categoryID])
+    });
+  }, [match.params.categoryName]);
 
   return (
     <Grid>
       <Grid container item>
         <Grid item>
           <Tooltip title="Back">
-            <IconButton
-              aria-label="back"
-              onClick={() => props.history.goBack()}
-            >
+            <IconButton aria-label="back" onClick={() => props.history.goBack()}>
               <ArrowBack />
             </IconButton>
           </Tooltip>
         </Grid>
         <Grid item>
-          <Typography variant="h4">{match.params.categoryName}</Typography>
+          <Typography variant="h4" align="center">
+            {match.params.categoryName}
+          </Typography>
         </Grid>
       </Grid>
 
-      {batches.map(batch => {
-        return (
-          <Link
-            underline="none"
-            component={RouterLink}
-            to={`${match.url}/${batch.batch_id}`}
-            key={batch.batch_id}
-          >
-            <Grid item>
-              <CouponInstance batch={batch} />
-            </Grid>
-          </Link>
-        )
-      })}
+      {batches.length ? (
+        batches.map(batch => {
+          return (
+            <Link
+              underline="none"
+              component={RouterLink}
+              to={`${match.url}/${batch.batch_id}`}
+              key={batch.batch_id}>
+              <Grid item>
+                <CouponInstance batch={batch} />
+              </Grid>
+            </Link>
+          );
+        })
+      ) : (
+        <div>
+          <Typography variant="h4" align="center">
+            <strong>Too bad!</strong>
+          </Typography>
+          <Typography variant="h5" align="center">
+            Seems like no coupons are available for {match.params.categoryName}. 😒
+          </Typography>
+          <Typography variant="h6" align="center">
+            Check back later!
+          </Typography>
+        </div>
+      )}
     </Grid>
-  )
+  );
 }
