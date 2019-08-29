ef0cae3a468a71b061359a615783550c6e81e7a5
Stavros <stavris.47@gmail.com>
2019-08-29 18:04:00 +0300

    Lift State up to parent

--- frontend/src/Components/Batches/BatchPage/SortFilter.jsx
+++ frontend/src/Components/Batches/BatchPage/SortFilter.jsx
@@ -1,28 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Select from 'react-select';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { Grid, Switch } from '@material-ui/core';
 
 export default function SortFilter(props) {
-  const [selectedOption, setSelectedOption] = useState(null);
-  const [filterRedeemed, setFilterRedeemed] = useState(false);
-
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' },
-  ];
-
-  const handleSortChange = selectedOption => {
-    setSelectedOption(selectedOption);
-    console.log(`Option selected:`, selectedOption);
-  };
-
-  const handleSwitch = name => event => {
-    //console.log(event);
-    setFilterRedeemed(event.target.checked);
-  };
-
   return (
     <Grid container item spacing={3}>
       <Grid item xs={6}>
@@ -31,17 +12,21 @@ export default function SortFilter(props) {
           labelPlacement="start"
           control={
             <Switch
-              checked={filterRedeemed}
-              onChange={handleSwitch('toggle')}
+              checked={props.filterRedeemed}
+              onChange={props.handleSwitch('toggle')}
               value="IsRedeemed"
               color="primary"
             />
           }
           label="Redeemed"
+          style={{ fontSize: '0.5em' }}
         />
       </Grid>
       <Grid item xs={6}>
-        <Select value={selectedOption} onChange={handleSortChange} options={options}></Select>
+        <Select
+          value={props.selectedOption}
+          onChange={props.handleSortChange}
+          options={props.options}></Select>
       </Grid>
     </Grid>
   );
