75030219f26511de447f5226fe0e657c6a2864be
Stavros <stavris.47@gmail.com>
2019-09-04 15:26:10 +0300

    Separate sort direction and remove redeemed switch

--- frontend/src/Components/Batches/SortFilterBatches.jsx
+++ frontend/src/Components/Batches/SortFilterBatches.jsx
@@ -1,41 +1,38 @@
 import React from 'react';
 import Select from 'react-select';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import { Grid, Switch } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
 export default function SortFilterBatches(props) {
   return (
     <Grid container item spacing={1}>
-      <Grid item xs={5}>
-        <FormControlLabel
-          size="small"
-          labelPlacement="top"
-          control={
-            <Switch
-              checked={props.filterRedeemed}
-              onChange={props.handleSwitch('toggle')}
-              value="IsRedeemed"
-              color="primary"
-            />
-          }
-          label="Redeemed"
-          style={{ fontSize: '0.5em' }}
-        />
-      </Grid>
       <Grid item xs={7}>
-        Sort
+        Sort by:
         <Select
           value={props.selectedSort}
           onChange={props.handleSortChange}
           options={props.sortOptions}></Select>
       </Grid>
-      <Grid item xs={12}>
-        Filter
+      <Grid item xs={5}>
+        Direction:
         <Select
-          isMulti
-          value={props.selectedFilter}
-          onChange={props.handleFilterChange}
-          options={props.filterOptions}></Select>
+          value={props.sortDirection}
+          onChange={props.handleSortDirChange}
+          options={props.dirOptions}></Select>
+      </Grid>
+
+      <Grid item xs={7}>
+        Status:
+        <Select
+          value={props.selectedStatus}
+          onChange={props.handleStatusChange}
+          options={props.statusOptions}></Select>
+      </Grid>
+      <Grid item xs={5}>
+        Type:
+        <Select
+          value={props.selectedType}
+          onChange={props.handleTypeChange}
+          options={props.typeOptions}></Select>
       </Grid>
     </Grid>
   );
