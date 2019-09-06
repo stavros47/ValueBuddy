ca59ddb48dcd1b24d9bd6f9f0cd0d194568b03d6
Stavros <stavris.47@gmail.com>
2019-09-06 15:39:24 +0300

    Rename and add new analytics widgets

--- frontend/src/Components/Dashboard/Components/index.js
+++ frontend/src/Components/Dashboard/Components/index.js
@@ -1,3 +1,5 @@
 export { default as TotalUsers } from './TotalUsers';
 export { default as CouponActivity } from './CouponActivity';
-export { default as BatchActivity } from './BatchActivity';
+export { default as LineActivity } from './LineActivity';
+export { default as CustCouponActivity } from './CustCouponActivity';
+export { default as FavouritePie } from './FavouritePie';
